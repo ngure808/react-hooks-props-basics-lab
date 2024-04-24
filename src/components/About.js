c4d8df19e96125fe11848fbd5dcd7d34b1a0c537
ngure808 <mwangingure808@gmail.com>
2024-04-24 12:41:34 +0300

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,24 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
-}
+function About(props) {
+  if (props.bio === undefined || props.bio === ""){
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      </div>
+    )
+  } else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{props.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links link1 = {props.link1} link2 = {props.link2}/> 
+      </div>
+    );
+  };
+};
 
 export default About;
